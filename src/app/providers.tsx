9828bc8b4d8c60a36382219e28337e87e11bc34a
Ayanava Karmakar <ayanava@rapidken.ai>
2023-07-01 19:26:09 +0530

    feat: add provider

---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+
+/**
+ * @see https://github.com/pacocoursey/next-themes#with-app
+ */
+export function Providers({ children }:{ children: React.ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>
+}
