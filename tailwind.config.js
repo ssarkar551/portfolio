04290e968218f7007566d9b08c00ca357c46dc68
ssarkar551 <ssarkar551@gmail.com>
2023-07-08 16:24:10 +0530

    feat: responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      gridTemplateColumns: {
+        sidebar: "300px auto", //for sidebar layout
+        "sidebar-collapsed": "64px auto", //for collapsed sidebar layout
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
